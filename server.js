2f71729e6bcd610cced83d2b825c10b08b3e8822
kat <katrinashui1992@gmail.com>
2023-02-20 17:23:16 +0800

    Git2

--- server.js
+++ server.js
@@ -177,7 +177,7 @@ app.post(
  }
 
  let endpointSecret;
-  // endpointSecret = "whsec_65d02c1bec75dc957e1a17522841f9ec9f497ef80d0d6408394a556094b943f7";
+  endpointSecret = "whsec_65d02c1bec75dc957e1a17522841f9ec9f497ef80d0d6408394a556094b943f7";
 
  app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
     const sig = request.headers['stripe-signature'];
